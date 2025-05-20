d6459dba60381331f618f3e484b470e88e447e33
Amresh <amreshbinary@gmail.com>
2025-05-20 21:50:20 +0530

    add notes

---
+++ Note.sql
@@ -0,0 +1,26 @@
+-- create database telusko_db;
+ use telusko_db;
+
+-- show databases; 
+
+-- create table personalInfo(
+-- id int ,
+-- name varchar(44) not null,
+-- age int not null,
+-- city varchar(60)
+-- );
+
+
+-- insert into personalInfo(id,name,age,city) values (3,"Ram",25,"Pune"), (4,"Suresh",26,"Pune"),(5,"Kalpesh",26,"Chandigarh");
+ -- Select * from personalInfo; 
+
+-- truncate table personalInfo;
+
+-- alter table personalInfo add constraint primary key (id);
+
+ -- select  * from personalInfo Where id Between 1 and 3 ;
+ -- select * from personalInfo order by city Asc;
+ -- select distinct  city from personalInfo;
+ -- update personalInfo Set age=25 where id = 1;
+ select * from personalInfo;
+ 
